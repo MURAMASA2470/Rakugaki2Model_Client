e5aaa31f7ab92e5f0b2b8df345cf8820039e86d7
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2019-12-20 16:08:52 +0900

    vuetifyのバージョンアップ

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,9 +63,11 @@ export default {
       info: colors.teal.lighten1,
       warning: colors.amber.base,
       error: colors.deepOrange.accent4,
-      success: colors.green.accent3
+      success: colors.green.accent3,
+      dark: false
     }
   },
+  
   /*
   ** Build configuration
   */
