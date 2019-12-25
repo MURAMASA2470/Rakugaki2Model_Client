d4aa13d7daccefd8a71ef55cb2f257a97a06fcae
MURAMASA2470 <keisuke226214@yahoo.co.jp>
2019-12-25 18:21:15 +0900

    色々修正した

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,9 @@
         </v-card>
         <div class="btn-group">
           <!-- <v-btn href="javascript:void(0)" class="v-btn info" id="download">保存</v-btn> -->
-          <v-btn href="javascript:void(0)" class="v-btn info" id="generate">画像生成</v-btn>
+          <v-btn href="javascript:void(0)" id="generate" class="v-btn info" @click="transfer()">画像生成</v-btn>
           <v-btn href="javascript:void(0)" id="clear" color="blue-grey" dark>クリア</v-btn>
-          <v-btn href="javascript:void(0)" class="v-btn error mt-1" @click="imagePut">デモ用</v-btn>
+          <v-btn href="javascript:void(0)" id="demo" class="v-btn error mt-1" @click="imagePut('./test.png')">デモ用</v-btn>
         </div>
 
         <v-dialog v-model="genDialog" width="256" height="300">
@@ -123,13 +123,13 @@
 </style>
 
 <script>
-import Viwer from "~/components/viewer.vue";
+import Viwer from "~/components/viewer.vue"
 
-const cvWidth = 256;
-const cvHeight = 256;
-const cvColor = "0,0,0,1";
-const cvBold = 1;
-const bgColor = "rgb(255,255,255)";
+const cvWidth = 256
+const cvHeight = 256
+const cvColor = "0,0,0,1"
+const cvBold = 1
+const bgColor = "rgb(255,255,255)"
 
 export default {
   head: {
@@ -142,6 +142,7 @@ export default {
       dlBtn: null,
       clrBtn: null,
       genBtn: null,
+      demoBtn: null,
       clickFlg: 0,
       dialog: false,
       genDialog: false,
@@ -152,118 +153,106 @@ export default {
       pix2pix: null,
       w: cvWidth,
       h: cvHeight
-    };
+    }
   },
   mounted() {
-    this.cv = document.getElementById("cv");
-    this.ctx = this.cv.getContext("2d");
+    this.cv = document.getElementById("cv")
+    this.ctx = this.cv.getContext("2d")
 
-    this.tmpcv = document.getElementById("tmpcv");
-    this.tmpctx = this.cv.getContext("2d");
+    this.tmpcv = document.getElementById("tmpcv")
+    this.tmpctx = this.cv.getContext("2d")
 
-    this.dlBtn = document.getElementById("download");
-    this.clrBtn = document.getElementById("clear");
-    this.genBtn = document.getElementById("generate");
-
-    
+    this.dlBtn = document.getElementById("download")
+    this.clrBtn = document.getElementById("clear")
+    this.genBtn = document.getElementById("generate")
+    this.demoBtn = document.getElementById("demo")
 
     // canvasの背景色を設定(指定がない場合にjpeg保存すると背景が黒になる)
-    this.setCvSize();
-    this.setBgColor();
+    this.setCvSize()
+    this.setBgColor()
 
     // canvas上でのイベント
-    this.cv.addEventListener("mousedown", () => {
-      this.clickFlg = 1;
-    });
-    this.cv.addEventListener("mouseup", () => {
-      this.clickFlg = 0;
-    });
-    this.cv.addEventListener("mousemove", e => {
-      if (!this.clickFlg) return false;
-      this.draw(e.offsetX, e.offsetY);
-    });
+    this.cv.addEventListener("mousedown", () => { this.clickFlg = 1 })
+    this.cv.addEventListener("mouseup", () => { this.clickFlg = 0 })
+    this.cv.addEventListener("mousemove", (e) => {
+      if (!this.clickFlg) return false
+      this.draw(e.offsetX, e.offsetY)
+    })
     // 描画クリア
     this.clrBtn.addEventListener("click", () => {
-      this.ctx.clearRect(0, 0, cvWidth, cvHeight);
-      this.setBgColor(bgColor);
-    });
+      this.ctx.clearRect(0, 0, cvWidth, cvHeight)
+      this.setBgColor(bgColor)
+    })
     // canvasを画像で保存
     // this.dlBtn.addEventListener('click', () => {
     //   this.dlBtn.href = this.cv.toDataURL("image/jpeg")
     //   this.dlBtn.download = 'komura.jpeg'
     // })
     // canvasの画像を使ってpix2pix
-    this.genBtn.addEventListener("click", () => {
-      let image = this.ctx.getImageData(0, 0, cvWidth, cvHeight);
-      this.tmpctx.putImageData(image, 0, 0);
-      this.genDialog = true;
-      console.log(this.tmpcv.toDataURL("image/jpeg"));
-      // エレメントが描写されるまで待つ
-      setTimeout(() => {
-        this.trnsdcv = document.getElementById("trnsdcv");
-        this.trnsdctx = this.trnsdcv.getContext("2d");
-        this.transfer();
+    // this.genBtn.addEventListener("click", this.transfer())
+    // this.demoBtn.addEventListener("click", this.imagePut("./test.png"))
 
-        // this.trnsdctx.drawImage(image, 0, 0, 650, 650, 0, 0, 256, 256)
-      }, 500);
-    });
-
-    //画像生成用キャンバスの準備
-
-    // console.log(this.trnsdcv)
     //pix2pixモデルのロード
-    console.log("ml5 ver: ", ml5.version);
+    console.log("ml5 ver: ", ml5.version)
     this.pix2pix = ml5.pix2pix("./chair.pict", () => {
-      console.log("Model Loaded.");
-    });
+      console.log("Model Loaded.")
+    })
   },
   methods: {
     // canvasの背景色を設定(指定がない場合にjpeg保存すると背景が黒になる)
     setBgColor: function(color = bgColor, w = cvWidth, h = cvHeight) {
-      this.ctx.fillStyle = color;
-      this.ctx.fillRect(0, 0, w, h);
+      this.ctx.fillStyle = color
+      this.ctx.fillRect(0, 0, w, h)
 
-      this.tmpctx.fillStyle = color;
-      this.tmpctx.fillRect(0, 0, 256, 256);
+      this.tmpctx.fillStyle = color
+      this.tmpctx.fillRect(0, 0, 256, 256)
     },
     // 描画処理
     draw: function(x, y) {
-      this.ctx.lineWidth = cvBold;
-      this.ctx.strokeStyle = "rgba(" + cvColor + ")";
+      this.ctx.lineWidth = cvBold
+      this.ctx.strokeStyle = "rgba(" + cvColor + ")"
       // 初回処理の判定
       if (this.clickFlg == "1") {
-        this.clickFlg = "2";
-        this.ctx.beginPath();
-        this.ctx.lineCap = "round"; //　線を角丸にする
-        this.ctx.moveTo(x, y);
+        this.clickFlg = "2"
+        this.ctx.beginPath()
+        this.ctx.lineCap = "round" //　線を角丸にする
+        this.ctx.moveTo(x, y)
       } else {
-        this.ctx.lineTo(x, y);
+        this.ctx.lineTo(x, y)
       }
-      this.ctx.stroke();
+      this.ctx.stroke()
     },
     setCvSize: function(w = cvWidth, h = cvHeight) {
-      this.cv.width = w;
-      this.cv.height = h;
-      this.cv.parentElement.style.width = w + "px";
-      this.cv.parentElement.style.height = h + "px";
-      document.getElementsByClassName("btn-group")[0].style.width = w + "px";
+      this.cv.width = w
+      this.cv.height = h
+      this.cv.parentElement.style.width = w + "px"
+      this.cv.parentElement.style.height = h + "px"
+      document.getElementsByClassName("btn-group")[0].style.width = w + "px"
 
-      // this.tmpcv.width = 256
-      // this.tmpcv.height = 256
     },
-    transfer(w = cvWidth, h = cvHeight) {
-      this.pix2pix.transfer(this.cv, (err, result) => {
-        console.log(result.src);
-        console.log("生成");
+    imagePut(img="./test.png") {
+      console.log(img)
+      let imgTest = new Image()
+      imgTest.src = img
 
-        this.trnsdctx.drawImage(result, 0, 0, 256, 256);
-      });
+      this.ctx.drawImage(imgTest, 0, 0, 256, 256)
     },
-    imagePut() {
-      let imgTest = new Image();
-      imgTest.src = "./test.png";
-      // console.log(imgTest)
-      this.ctx.drawImage(imgTest, 0, 0, 256, 256);
+    transfer() {
+      // let image = this.ctx.getImageData(0, 0, cvWidth, cvHeight)
+      // this.tmpctx.putImageData(image, 0, 0)
+      // console.log(this.tmpcv.toDataURL("image/jpeg"))
+      this.genDialog = true
+      console.log('transfer')
+      // エレメントが描写されるまで待つ
+      setTimeout(() => {
+        this.trnsdcv = document.getElementById("trnsdcv")
+        this.trnsdctx = this.trnsdcv.getContext("2d")
+
+        this.pix2pix.transfer(this.cv, (err, result) => {
+          console.log(result)
+        })
+        // this.trnsdctx.drawImage(image, 0, 0, 650, 650, 0, 0, 256, 256)
+      }, 500)
     }
   },
   components: {
